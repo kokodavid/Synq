1ffd8976cfb20b40a86719eecffc256b7ee10894
kokodavid <kingkgdave@gmail.com>
2024-09-18 00:07:49 +0300

    initial commit

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomePageScreen extends ConsumerStatefulWidget {
+  const HomePageScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends ConsumerState<HomePageScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
